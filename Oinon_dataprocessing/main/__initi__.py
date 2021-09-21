c4acb3d610c6c8f05347668a21811f9cec5f8d85
Onion653 <1498443003@qq.com>
2021-09-21 22:44:16 +0800

    first

--- Oinon_dataprocessing/main/__initi__.py
+++
@@ -1,1 +0,0 @@
-from Oinon_dataprocessing import atand,linfit
--- Oinon_dataprocessing/main/atand.py
+++
@@ -1,9 +0,0 @@
-def atand(list): #输入项可以是列表或者元组 
-    number_a=-len(list)
-    print("这里有"+str(-number_a)+"个数据")
-    str_b=" "
-    while number_a<0:
-        number_c=list[number_a]
-        str_b=str_b+"&"+str(number_c)
-        number_a=number_a+1
-    return str_b
